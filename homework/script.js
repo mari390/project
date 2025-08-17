81bb531b3de40ef7585e0efc62f804805cf6129b
Mari <mari@mbonner.com>
2025-08-17 08:46:09 -0700

    java script homework

---
+++ homework/script.js
@@ -0,0 +1,30 @@
+const todos_data = [
+  {
+    id: 1,
+    title: "finish the HW",
+    priority: 1,
+    desc: "Math",
+    isDone: true,
+  },
+  {
+    id: 2,
+    title: "Practice piano",
+    priority: 3,
+    desc: "play song 4 times",
+    isDone: false,
+  },
+  {
+    id: 3,
+    title: "complete summer reading book",
+    priority: 3,
+    desc: 'Read the book: "White Noise"',
+    isDone: true,
+  },
+  {
+    id: 4,
+    title: "Excercise",
+    priority: 2,
+    desc: "Go for a run",
+    isDone: true,
+  },
+];
